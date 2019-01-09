2075e2a2764e25ec83fe1e0f4fc6bd30d57fc785
Vipin Yadav <imvpn22@gmail.com>
2019-01-09 18:14:19 +0530

    Updates

---
+++ precache-manifest.4c10943877618e248c7ed3abeaebd30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "6e3a72a7358e5c6b46cd",
+    "url": "/todo/static/js/main.6e3a72a7.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/todo/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "6e3a72a7358e5c6b46cd",
+    "url": "/todo/static/css/main.a9912df3.chunk.css"
+  },
+  {
+    "revision": "0ad4faff6b0e0c456fc5eea25c8b4c70",
+    "url": "/todo/index.html"
+  }
+];
