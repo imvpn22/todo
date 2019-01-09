44aa8f5334959fe8f1d646b59b3c65f2871a0330
Vipin Yadav <imvpn22@gmail.com>
2019-01-09 16:54:42 +0530

    UI Update & Refactor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,118 @@ import React, { Component } from 'react';
 import TodoList from './TodoList';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.newItemRef = React.createRef();
+        this.state = {
+            currentItem: {text:'', id:'', done:false},
+            items: [{
+                    id: 1,
+                    text: 'Complete this project',
+                    done: false
+                }, {
+                    id: 2,
+                    text: 'Push on Github',
+                    done: false
+                }, {
+                    id: 3,
+                    text: 'Think about idea',
+                    done: true
+                }, {
+                    id: 4,
+                    text: 'Add some more style',
+                    done: false,
+                    editable: false
+                }, {
+                    id: 5,
+                    text: 'What to do',
+                    done: true
+                }, {
+                    id: 6,
+                    text: 'What to do',
+                    done: true
+                }
+            ]
+        }
+    }
+
+    // componentDidMount() {
+    //     this.newItemRef.focus()
+    // }
+
+    addItem = (e) => {
+        e.preventDefault();
+        let newItem = this.state.currentItem;
+        if (!!newItem.text) {
+            let items = [...this.state.items, newItem]
+            this.setState({
+                items: items,
+                currentItem: { text: '', key: '', done:false }
+            });
+        }
+
+    };
+
+    handleItemChange = e => {
+        let text = e.target.value;
+        let currentItem = {
+            text: text,
+            id: Date.now()
+        };
+        this.setState({currentItem});
+    };
+
+    doneItem = (itemId) => {
+        let items = this.state.items;
+        items = items.map(item => {
+            if (itemId === item.id) {
+                item.done = !item.done;
+            }
+            return item;
+        });
+        this.setState({items});
+    };
+
+    deleteItem = (itemId) => {
+        let items = this.state.items;
+        items = items.filter(item => itemId !== item.id);
+        this.setState({items});
+    };
+
+    editItem = (itemId) => {
+        let items = this.state.items;
+        items = items.map(item => {
+            if (item.id === itemId) item.editable = !item.editable;
+            return item;
+        });
+        this.setState({items});
+    };
+
+    activeEdit = (itemId, e) => {
+        let text = e.target.value;
+        let items = this.state.items;
+        items = items.map(item => {
+            if (item.id === itemId) item.text = text;
+            return item;
+        });
+        this.setState({items});
+    };
+
+
   render() {
     return (
         <div className="container">
-            <TodoList />
+            <TodoList
+                items={this.state.items}
+                currentItem={this.state.currentItem}
+                handleItemChange={this.handleItemChange}
+                addItem={this.addItem}
+                doneItem={this.doneItem}
+                deleteItem={this.deleteItem}
+                editItem={this.editItem}
+                activeEdit={this.activeEdit}
+            />
         </div>
         );
     }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,121 +1,54 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.newItemRef = React.createRef();
-        this.state = {
-            currentItem: {text:'', id:'', done:false},
-            items: [{
-                    id: 1,
-                    text: 'Complete this project kshkjdshfkjdhfkhthis project kshkjdshfkjdhfkhthis project kshkjdshfkjdhfkhthis project kshkjdshfkjdhfkh',
-                    done: false
-                }, {
-                    id: 2,
-                    text: 'Push on Github',
-                    done: false
-                }, {
-                    id: 3,
-                    text: 'Think about idea',
-                    done: true
-                }, {
-                    id: 4,
-                    text: 'What to do',
-                    done: true
-                }, {
-                    id: 5,
-                    text: 'What to do',
-                    done: true
-                }, {
-                    id: 6,
-                    text: 'What to do',
-                    done: true
-                }
-            ]
-        }
-    }
 
-    componentDidUpdate() {
-        this.newItemRef.current.focus()
-    }
-
-    addItem = (e) => {
-        e.preventDefault();
-        let newItem = this.state.currentItem;
-        if (!!newItem.text) {
-            let items = [...this.state.items, newItem]
-            this.setState({
-                items: items,
-                currentItem: { text: '', key: '', done:false }
-            });
-            // this.newItemRef.clear();
-        }
-
-    };
-
-    handleItemChange = e => {
-        let text = e.target.value;
-        let currentItem = {
-            text: text,
-            id: Date.now()
-        };
-        this.setState({currentItem});
-    };
-
-    doneItem = (itemId) => {
-        let items = this.state.items;
-        items = items.map(item => {
-            if (itemId === item.id) {
-                item.done = !item.done;
-            }
-            return item;
-        });
-        this.setState({items});
-    };
-
-    deleteItem = (itemId) => {
-        let items = this.state.items;
-        items = items.filter(item => itemId !== item.id);
-        this.setState({items});
-    };
 
     createItem = (item) => {
-        return <div className={"item " + (item.done ? "doneItem" : "activeItem")} key={item.id}>
-                <label onClick={() => this.doneItem(item.id)}>
+        if (item.editable) {
+            return <div className={"item " + (item.done ? "doneItem" : "activeItem")} key={item.id}>
+                <label>
+                    <i className={"far " + (item.done ? "fa-check-circle" : "fa-circle")}></i>
+                    <input type="text" value={item.text} onChange={e => this.props.activeEdit(item.id, e)} autoFocus={true}/>
+                </label>
+                <span>
+                    <button onClick={() => this.props.editItem(item.id)}><i className="far fa-save"></i></button>
+                    <button onClick={() => this.props.deleteItem(item.id)}><i className="far fa-trash-alt"></i></button>
+                </span>
+            </div>
+        } else {
+            return <div className={"item " + (item.done ? "doneItem" : "activeItem")} key={item.id}>
+                <label onClick={() => this.props.doneItem(item.id)}>
                     <i className={"far " + (item.done ? "fa-check-circle" : "fa-circle")}></i>
                     <span>{item.text}</span>
                 </label>
                 <span>
-                    <button><i className="fas fa-pencil-alt"></i></button>
-                    <button onClick={() => this.deleteItem(item.id)}><i className="far fa-trash-alt"></i></button>
+                    <button onClick={() => this.props.editItem(item.id)}><i className="fas fa-pencil-alt"></i></button>
+                    <button onClick={() => this.props.deleteItem(item.id)}><i className="far fa-trash-alt"></i></button>
                 </span>
             </div>
-    }
+        }
+    };
 
     render() {
 
-        let items = this.state.items;
+        let items = this.props.items;
         let listItems = items.filter(item => !item.done).map(item => this.createItem(item));
         let doneItems = items.filter(item => item.done).map(item => this.createItem(item));
 
         return (
             <div className = "main">
                 <div className="header">
-                    <h2>ToDo App</h2>
+                    ToDo App
                 </div>
                 <div className="itemList">
-                    <h3>Add Item</h3>
-                    <form className="newItem item" onSubmit={e => this.addItem(e)}>
-                        <input ref={this.newItemRef} onChange={e => this.handleItemChange(e)} type="text" placeholder="Add new item..." required />
-                        <button type="submit">Add</button>
+                    <form className="newItem item" onSubmit={e => this.props.addItem(e)}>
+                        <input ref={input => this.newItemRef = input}
+                                value={this.props.currentItem.text}
+                                onChange={e => this.props.handleItemChange(e)}
+                                type="text" placeholder="Add new item..." required />
+                        <button type="submit"><i className="fas fa-check"></i></button>
                     </form>
-                </div>
-                <div className="itemList">
-                    *<h3>TODO</h3>
                     {listItems}
-                </div>
-                <div className="itemList">
-                    <h3>Completed</h3>
                     {doneItems}
                 </div>
             </div>
