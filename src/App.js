62766e0510b2968f725235c9b1bf5182806e5d75
Vipin Yadav <imvpn22@gmail.com>
2021-01-25 12:30:42 +0530

    added localstorage

--- src/App.js
+++ src/App.js
@@ -1,128 +1,127 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
 
+const DUMMY_DATA = [
+  {
+    id: 1,
+    text: 'Dummy 1',
+    done: true
+  },
+  {
+    id: 8,
+    text: 'Dummy 2',
+    done: false
+  }
+];
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.newItemRef = React.createRef();
-        this.state = {
-            currentItem: {text:'', id:'', done:false},
-            items: [{
-                    id: 1,
-                    text: 'Think about idea',
-                    done: true
-                }, {
-                    id: 2,
-                    text: 'Publish on Github',
-                    done: true
-                }, {
-                    id: 3,
-                    text: 'Add style',
-                    done: true
-                }, {
-                    id: 4,
-                    text: 'What to do',
-                    done: true
-                }, {
-                    id: 5,
-                    text: 'What to do',
-                    done: true
-                }, {
-                    id: 6,
-                    text: 'Optimize for mobile view',
-                    done: false
-                }, {
-                    id: 7,
-                    text: 'Add PWA features',
-                    done: false
-                }, {
-                    id: 8,
-                    text: 'Complete this project',
-                    done: false
-                },
-            ]
-        }
+  constructor(props) {
+    super(props);
+    this.newItemRef = React.createRef();
+    this.state = {
+      currentItem: { text: '', id: '', done: false },
+      items: DUMMY_DATA
+    };
+  }
+
+  componentDidMount() {
+    this.newItemRef.current.focus();
+    const ls = localStorage.getItem('todos');
+    let todos = [];
+
+    try {
+      todos = JSON.parse(ls);
+    } catch (e) {
+      console.log(e);
     }
 
-    componentDidMount() {
-        this.newItemRef.current.focus()
+    if (todos && todos.length) {
+      this.setState({ items: todos });
     }
+  }
 
-    addItem = (e) => {
-        e.preventDefault();
-        let newItem = this.state.currentItem;
-        if (!!newItem.text) {
-            let items = [...this.state.items, newItem]
-            this.setState({
-                items: items,
-                currentItem: { text: '', key: '', done:false }
-            });
-        }
+  addItem = e => {
+    e.preventDefault();
+    let newItem = this.state.currentItem;
+    if (!!newItem.text) {
+      let items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '', done: false }
+      });
+      this.updateLocalStorage(items);
+    }
+  };
+
+  handleItemChange = e => {
+    let text = e.target.value;
+    let currentItem = {
+      text: text,
+      id: Date.now()
     };
+    this.setState({ currentItem });
+  };
 
-    handleItemChange = e => {
-        let text = e.target.value;
-        let currentItem = {
-            text: text,
-            id: Date.now()
-        };
-        this.setState({currentItem});
-    };
+  doneItem = itemId => {
+    let items = this.state.items;
+    items = items.map(item => {
+      if (itemId === item.id) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    this.setState({ items });
+    this.updateLocalStorage(items);
+  };
 
-    doneItem = (itemId) => {
-        let items = this.state.items;
-        items = items.map(item => {
-            if (itemId === item.id) {
-                item.done = !item.done;
-            }
-            return item;
-        });
-        this.setState({items});
-    };
+  deleteItem = itemId => {
+    let items = this.state.items;
+    items = items.filter(item => itemId !== item.id);
+    this.setState({ items });
+    this.updateLocalStorage(items);
+  };
 
-    deleteItem = (itemId) => {
-        let items = this.state.items;
-        items = items.filter(item => itemId !== item.id);
-        this.setState({items});
-    };
+  editItem = itemId => {
+    let items = this.state.items;
+    items = items.map(item => {
+      if (item.id === itemId) item.editable = !item.editable;
+      return item;
+    });
+    this.setState({ items });
+    this.updateLocalStorage(items);
+  };
 
-    editItem = (itemId) => {
-        let items = this.state.items;
-        items = items.map(item => {
-            if (item.id === itemId) item.editable = !item.editable;
-            return item;
-        });
-        this.setState({items});
-    };
+  activeEdit = (itemId, e) => {
+    let text = e.target.value;
+    let items = this.state.items;
+    items = items.map(item => {
+      if (item.id === itemId) item.text = text;
+      return item;
+    });
+    this.setState({ items });
+    this.updateLocalStorage(items);
+  };
 
-    activeEdit = (itemId, e) => {
-        let text = e.target.value;
-        let items = this.state.items;
-        items = items.map(item => {
-            if (item.id === itemId) item.text = text;
-            return item;
-        });
-        this.setState({items});
-    };
-
+  updateLocalStorage = data => {
+    localStorage.setItem('todos', JSON.stringify(data));
+  };
 
   render() {
     return (
-        <div className="container">
-            <TodoList
-                items={this.state.items}
-                newItemRef={this.newItemRef}
-                currentItem={this.state.currentItem}
-                handleItemChange={this.handleItemChange}
-                addItem={this.addItem}
-                doneItem={this.doneItem}
-                deleteItem={this.deleteItem}
-                editItem={this.editItem}
-                activeEdit={this.activeEdit}
-            />
-        </div>
-        );
-    }
+      <div className="container">
+        <TodoList
+          items={this.state.items}
+          newItemRef={this.newItemRef}
+          currentItem={this.state.currentItem}
+          handleItemChange={this.handleItemChange}
+          addItem={this.addItem}
+          doneItem={this.doneItem}
+          deleteItem={this.deleteItem}
+          editItem={this.editItem}
+          activeEdit={this.activeEdit}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,58 +1,91 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
-
-    createItem = (item) => {
-        if (item.editable) {
-            return <div className={"item " + (item.done ? "doneItem" : "activeItem")} key={item.id}>
-                <label>
-                    <i className={"far " + (item.done ? "fa-check-circle" : "fa-circle")}></i>
-                    <input type="text" value={item.text} onChange={e => this.props.activeEdit(item.id, e)} autoFocus={true}/>
-                </label>
-                <span>
-                    <button onClick={() => this.props.editItem(item.id)}><i className="far fa-save"></i></button>
-                    <button onClick={() => this.props.deleteItem(item.id)}><i className="far fa-trash-alt"></i></button>
-                </span>
-            </div>
-        } else {
-            return <div className={"item " + (item.done ? "doneItem" : "activeItem")} key={item.id}>
-                <label onClick={() => this.props.doneItem(item.id)}>
-                    <i className={"far " + (item.done ? "fa-check-circle" : "fa-circle")}></i>
-                    <span>{item.text}</span>
-                </label>
-                <span>
-                    <button onClick={() => this.props.editItem(item.id)}><i className="fas fa-pencil-alt"></i></button>
-                    <button onClick={() => this.props.deleteItem(item.id)}><i className="far fa-trash-alt"></i></button>
-                </span>
-            </div>
-        }
-    };
-
-    render() {
-
-        let items = this.props.items;
-        let listItems = items.filter(item => !item.done).map(item => this.createItem(item));
-        let doneItems = items.filter(item => item.done).map(item => this.createItem(item));
-
-        return (
-            <div className = "main">
-                <div className="header">
-                    ToDo
-                </div>
-                <div className="itemList">
-                    <form className="newItem item" onSubmit={e => this.props.addItem(e)}>
-                        <input ref={this.props.newItemRef}
-                                value={this.props.currentItem.text}
-                                onChange={e => this.props.handleItemChange(e)}
-                                type="text" placeholder="Add new item" required />
-                        <button type="submit"><i className="fas fa-check"></i></button>
-                    </form>
-                    {listItems}
-                    {doneItems}
-                </div>
-            </div>
-        )
+  createItem = item => {
+    if (item.editable) {
+      return (
+        <div
+          className={'item ' + (item.done ? 'doneItem' : 'activeItem')}
+          key={item.id}
+        >
+          <label>
+            <i
+              className={'far ' + (item.done ? 'fa-check-circle' : 'fa-circle')}
+            />
+            <input
+              type="text"
+              value={item.text}
+              onChange={e => this.props.activeEdit(item.id, e)}
+              autoFocus={true}
+            />
+          </label>
+          <span>
+            <button onClick={() => this.props.editItem(item.id)}>
+              <i className="far fa-save" />
+            </button>
+            <button onClick={() => this.props.deleteItem(item.id)}>
+              <i className="far fa-trash-alt" />
+            </button>
+          </span>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={'item ' + (item.done ? 'doneItem' : 'activeItem')}
+          key={item.id}
+        >
+          <label onClick={() => this.props.doneItem(item.id)}>
+            <i
+              className={'far ' + (item.done ? 'fa-check-circle' : 'fa-circle')}
+            />
+            <span>{item.text}</span>
+          </label>
+          <span>
+            <button onClick={() => this.props.editItem(item.id)}>
+              <i className="fas fa-pencil-alt" />
+            </button>
+            <button onClick={() => this.props.deleteItem(item.id)}>
+              <i className="far fa-trash-alt" />
+            </button>
+          </span>
+        </div>
+      );
     }
+  };
+
+  render() {
+    let items = this.props.items;
+    let listItems = items
+      .filter(item => !item.done)
+      .map(item => this.createItem(item));
+    let doneItems = items
+      .filter(item => item.done)
+      .map(item => this.createItem(item));
+
+    return (
+      <div className="main">
+        <div className="header">ToDo</div>
+        <div className="itemList">
+          <form className="newItem item" onSubmit={e => this.props.addItem(e)}>
+            <input
+              ref={this.props.newItemRef}
+              value={this.props.currentItem.text}
+              onChange={e => this.props.handleItemChange(e)}
+              type="text"
+              placeholder="Add new item"
+              required
+            />
+            <button type="submit">
+              <i className="fas fa-check" />
+            </button>
+          </form>
+          {listItems}
+          {doneItems}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default TodoList
+export default TodoList;
