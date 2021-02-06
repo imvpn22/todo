89bbe3c1d77aea911d0ed538202443bb78aec1ad
Vipin Yadav <imvpn22@gmail.com>
2021-02-06 16:03:10 +0530

    Integrated API calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import TodoList from './TodoList';
 
-const DUMMY_DATA = [
-  {
-    id: 1,
-    text: 'Dummy 1',
-    done: true
-  },
-  {
-    id: 8,
-    text: 'Dummy 2',
-    done: false
-  }
-];
+// const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://imvpn22-todo.herokuapp.com/api';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.newItemRef = React.createRef();
     this.state = {
-      currentItem: { text: '', id: '', done: false },
-      items: DUMMY_DATA
+      currentItem: {},
+      items: []
     };
   }
 
@@ -37,26 +29,24 @@ class App extends Component {
     if (todos && todos.length) {
       this.setState({ items: todos });
     }
+
+    // call
+    this.getTodosFromBackend();
   }
 
-  addItem = e => {
-    e.preventDefault();
-    let newItem = this.state.currentItem;
-    if (!!newItem.text) {
-      let items = [...this.state.items, newItem];
-      this.setState({
-        items: items,
-        currentItem: { text: '', key: '', done: false }
-      });
-      this.updateLocalStorage(items);
-    }
+  addItem = text => {
+    const newItem = { text };
+    this.addTodoInBackend(newItem);
+
+    const { items } = this.state;
+    newItem._id = Date.now();
+    items.push(newItem);
+    this.setState({ items });
   };
 
-  handleItemChange = e => {
-    let text = e.target.value;
+  handleItemChange = text => {
     let currentItem = {
-      text: text,
-      id: Date.now()
+      text: text
     };
     this.setState({ currentItem });
   };
@@ -64,26 +54,26 @@ class App extends Component {
   doneItem = itemId => {
     let items = this.state.items;
     items = items.map(item => {
-      if (itemId === item.id) {
-        item.done = !item.done;
+      if (itemId === item._id) {
+        item.isDone = !item.isDone;
+        this.updateTodoInBackend(item);
       }
       return item;
     });
     this.setState({ items });
-    this.updateLocalStorage(items);
+    // this.updateLocalStorage(items);
   };
 
   deleteItem = itemId => {
-    let items = this.state.items;
-    items = items.filter(item => itemId !== item.id);
+    this.deleteTodoFromBackend(itemId);
+    const items = this.state.items.filter(item => itemId !== item._id);
     this.setState({ items });
-    this.updateLocalStorage(items);
   };
 
   editItem = itemId => {
     let items = this.state.items;
     items = items.map(item => {
-      if (item.id === itemId) item.editable = !item.editable;
+      if (item._id === itemId) item.editable = !item.editable;
       return item;
     });
     this.setState({ items });
@@ -94,15 +84,63 @@ class App extends Component {
     let text = e.target.value;
     let items = this.state.items;
     items = items.map(item => {
-      if (item.id === itemId) item.text = text;
+      if (item._id === itemId) item.text = text;
       return item;
     });
     this.setState({ items });
-    this.updateLocalStorage(items);
+    // this.updateLocalStorage(items);
   };
 
   updateLocalStorage = data => {
     localStorage.setItem('todos', JSON.stringify(data));
+  };
+
+  getTodosFromBackend = () => {
+    axios
+      .get(API_BASE_URL + '/todos')
+      .then(res => {
+        const { todos } = res.data;
+        this.setState({ items: todos });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  updateTodoInBackend = todo => {
+    axios
+      .put(API_BASE_URL + '/todos', todo)
+      .then(res => {
+        console.log(res);
+        // const { todos } = res.data;
+        // this.setState({ items: todos });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  addTodoInBackend = data => {
+    axios
+      .post(API_BASE_URL + '/todos', data)
+      .then(res => {
+        this.getTodosFromBackend();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  deleteTodoFromBackend = todoId => {
+    axios
+      .delete(API_BASE_URL + '/todos/' + todoId)
+      .then(res => {
+        // console.log('delete success', res.data);
+        this.getTodosFromBackend();
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
-  createItem = item => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: ''
+    };
+  }
+
+  updateCurrentText = e => {
+    const inputText = e.target.value;
+    this.setState({ inputText });
+  };
+
+  createTodoItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.inputText);
+  };
+
+  createItemHtml = item => {
     if (item.editable) {
       return (
         <div
-          className={'item ' + (item.done ? 'doneItem' : 'activeItem')}
-          key={item.id}
+          className={'item ' + (item.isDone ? 'doneItem' : 'activeItem')}
+          key={item._id}
         >
           <label>
             <i
-              className={'far ' + (item.done ? 'fa-check-circle' : 'fa-circle')}
+              className={
+                'far ' + (item.isDone ? 'fa-check-circle' : 'fa-circle')
+              }
             />
             <input
               type="text"
               value={item.text}
-              onChange={e => this.props.activeEdit(item.id, e)}
+              onChange={e => this.props.activeEdit(item._id, e)}
               autoFocus={true}
             />
           </label>
           <span>
-            <button onClick={() => this.props.editItem(item.id)}>
+            <button onClick={() => this.props.editItem(item._id)}>
               <i className="far fa-save" />
             </button>
-            <button onClick={() => this.props.deleteItem(item.id)}>
+            <button onClick={() => this.props.deleteItem(item._id)}>
               <i className="far fa-trash-alt" />
             </button>
           </span>
@@ -32,20 +51,22 @@ class TodoList extends Component {
     } else {
       return (
         <div
-          className={'item ' + (item.done ? 'doneItem' : 'activeItem')}
-          key={item.id}
+          className={'item ' + (item.isDone ? 'doneItem' : 'activeItem')}
+          key={item._id}
         >
-          <label onClick={() => this.props.doneItem(item.id)}>
+          <label onClick={() => this.props.doneItem(item._id)}>
             <i
-              className={'far ' + (item.done ? 'fa-check-circle' : 'fa-circle')}
+              className={
+                'far ' + (item.isDone ? 'fa-check-circle' : 'fa-circle')
+              }
             />
             <span>{item.text}</span>
           </label>
           <span>
-            <button onClick={() => this.props.editItem(item.id)}>
+            <button onClick={() => this.props.editItem(item._id)}>
               <i className="fas fa-pencil-alt" />
             </button>
-            <button onClick={() => this.props.deleteItem(item.id)}>
+            <button onClick={() => this.props.deleteItem(item._id)}>
               <i className="far fa-trash-alt" />
             </button>
           </span>
@@ -57,21 +78,21 @@ class TodoList extends Component {
   render() {
     let items = this.props.items;
     let listItems = items
-      .filter(item => !item.done)
-      .map(item => this.createItem(item));
+      .filter(item => !item.isDone)
+      .map(item => this.createItemHtml(item));
     let doneItems = items
-      .filter(item => item.done)
-      .map(item => this.createItem(item));
+      .filter(item => item.isDone)
+      .map(item => this.createItemHtml(item));
 
     return (
       <div className="main">
         <div className="header">ToDo</div>
         <div className="itemList">
-          <form className="newItem item" onSubmit={e => this.props.addItem(e)}>
+          <form className="newItem item" onSubmit={this.createTodoItem}>
             <input
               ref={this.props.newItemRef}
               value={this.props.currentItem.text}
-              onChange={e => this.props.handleItemChange(e)}
+              onChange={e => this.updateCurrentText(e)}
               type="text"
               placeholder="Add new item"
               required
